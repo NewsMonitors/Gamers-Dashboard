1b648dd5133f23e9a92f27f8821b7f756965a0f2
Edward Lind <david.berg333@hotmail.com>
2017-03-09 21:13:42 +0100

    search result created

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,23 +1,67 @@
 'use strict';
 
 angular.module('gamersDashApp')
-  .controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
+    .controller('NavbarCtrl', function($scope, $rootScope, $location, Auth) {
+        $scope.menu = [{
+            'title': 'Home',
+            'link': '/'
+        }];
 
-    $scope.isCollapsed = true;
-    $scope.isLoggedIn = Auth.isLoggedIn;
-    $scope.isAdmin = Auth.isAdmin;
-    $scope.getCurrentUser = Auth.getCurrentUser;
+        $scope.isCollapsed = true;
+        $scope.isLoggedIn = Auth.isLoggedIn;
+        $scope.isAdmin = Auth.isAdmin;
+        $scope.getCurrentUser = Auth.getCurrentUser;
 
-    $scope.logout = function() {
-      Auth.logout();
-      $location.path('/login');
-    };
+        $scope.logout = function() {
+            Auth.logout();
+            $location.path('/login');
+        };
 
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+        $scope.isActive = function(route) {
+            return route === $location.path();
+        };
+
+        //submit search form
+        $scope.topBarSearch = function(searchText) {
+            var searchType = $(".header-search-form button").find('.btn-value').text();
+            var selectedItemIndex; 
+            switch (searchType) {
+                case 'Everything':
+                    selectedItemIndex = 0;
+                    break;
+                case 'Games':
+                    selectedItemIndex = 1;
+                    break;
+                case 'Gamertags':
+                    selectedItemIndex = 2;
+                    break;
+                case 'Achievements':
+                    selectedItemIndex = 3;
+                    break;
+                case 'Leaderboards':
+                    selectedItemIndex = 4;
+                    break;
+            }
+            var dataToMainCtrl = {
+                'searchText': searchText,
+                'selectedItemIndex': selectedItemIndex
+            };
+            //initialize the second search bar filter
+            $(".dropdown-menu-filters>li").each(function() {
+                $(this).removeClass("off");
+                $(this).find('input').val(1);
+            });
+            $rootScope.$broadcast('$topBarSearchResult', dataToMainCtrl);
+            //empty the search input value
+            $scope.searchText = '';
+            $('input.header-search-query').blur();
+        }
+        //jquery for drop down search
+        $(".header-search-select>li>a").on("click", function(e) {
+            e.preventDefault();
+            $(this).parents(".header-search-form").find('.btn-value').text($(this).text());
+            $(this).parents(".header-search-form").find('.header-search-category').val($(this).data('category'));
+            $(this).parents(".header-search-form").find('input[name="group"]').val($(this).data('category'));
+        });
+
+    });
